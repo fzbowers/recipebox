da66119599e1d6ca09db42b3f9f4687b12064b38
Fatima Bowers <bowersfatima@gmail.com>
2025-03-23 03:56:05 -0400

    update db initialization

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -26,7 +26,7 @@ def create_app():
     app.register_blueprint(features_routes.bp)
     # app.register_blueprint(auth.auth_bp)
 
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
 
     return app
